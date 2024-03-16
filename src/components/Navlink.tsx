93a09dd8033e5d043aaf0d31602bbda2b5f79832
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-16 15:51:55 -0600

    Header

---
+++ src/components/Navlink.tsx
@@ -0,0 +1,41 @@
+import { PropsWithChildren } from 'react';
+import styles from './Navlink.module.css';
+import { NavLink } from 'react-router-dom';
+
+type Props = {
+  to: string;
+  styleIndex: number;
+  end?: boolean;
+};
+
+const activeStyle = {
+  textDecoration: 'underline',
+  color: '#161616',
+};
+
+const activeFontWeights = [600];
+
+export default function Navlink({
+  to,
+  styleIndex,
+  end = false,
+  children,
+}: PropsWithChildren<Props>): JSX.Element {
+  return (
+    <NavLink
+      to={to}
+      className={styles.navlink + ' ' + styles[`navlink-${styleIndex}`]}
+      style={({ isActive }) =>
+        isActive
+          ? {
+              ...activeStyle,
+              fontWeight: activeFontWeights[styleIndex],
+            }
+          : {}
+      }
+      end={end}
+    >
+      {children}
+    </NavLink>
+  );
+}
