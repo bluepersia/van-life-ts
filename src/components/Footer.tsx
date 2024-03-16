d17994b94309699057f14b6d6b764e1b3472ad21
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-16 15:55:01 -0600

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className={styles.footer}>
+      <p>Ⓒ 2022 #VANLIFE</p>
+    </footer>
+  );
+}
