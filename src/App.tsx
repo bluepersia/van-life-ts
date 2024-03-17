43e809d2e26fb85f0ef00d3f7782848552601dc0
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-16 18:06:48 -0600

    Van Detail

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans.js';
+import VanDetail from './pages/VanDetail.js';
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path='about' element={<About />} />
             <Route path='vans' element={<Vans />} />
+            <Route path='vans/:id' element={<VanDetail />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/BackBtn.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren } from 'react';
+import styles from './BackBtn.module.css';
+import { Link } from 'react-router-dom';
+import imgArrow from '../img/Arrow.png';
+type Props = {
+  to?: string;
+};
+
+export default function BackBtn({
+  to = '..',
+  children,
+}: PropsWithChildren<Props>): JSX.Element {
+  return (
+    <Link to={to} relative='path' className={styles.btn}>
+      <img src={imgArrow} /> <span> {children}</span>
+    </Link>
+  );
+}
