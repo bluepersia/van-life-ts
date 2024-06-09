59bb88b43b3293c4c524e69b8335a08583e6452a
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-06-09 13:26:31 -0600

    Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export default function Home(): JSX.Element {
+  return (
+    <main className='home'>
+      <h1 className='home__title'>
+        You got the travel plans, we got the travel vans.
+      </h1>
+      <p className='home__body'>
+        Add adventure to your life by joining the #vanlife movement. Rent the
+        perfect van to make your perfect road trip.
+      </p>
+      <button className='home__btn'>Find your van</button>
+    </main>
+  );
+}
