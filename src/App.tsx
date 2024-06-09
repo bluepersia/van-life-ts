0d190601e475a4652bc1606d6318963254d36659
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-06-09 13:06:31 -0600

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Footer />
+      <Routes>
+        <Route path='/' element={<Layout />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
